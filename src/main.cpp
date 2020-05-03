a5647203bd2586de65f2c4d8dfd09a7ea13c906f
Sanmathi Bharamgouda <sanmathi.bharamgouda@leiainc.com>
2020-05-03 02:06:18 -0700

    Load frame function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <GLFW/glfw3.h>
 
+bool load_frame(const char* filename, int* widht, int* height, unsigned char** data);
+
 int main (int argc, const char** argv){
 
     GLFWwindow* window;
@@ -12,7 +14,7 @@ int main (int argc, const char** argv){
 
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 
-    unsigned char* data = new unsigned char[100 * 100 * 3];
+/*     unsigned char* data = new unsigned char[100 * 100 * 3];
     for (int x = 0; x < 100; ++x)
     {
         for (int y = 0; y < 100; ++y)
@@ -30,25 +32,34 @@ int main (int argc, const char** argv){
             data[x * 100 * 3 + y * 3 + 1 ] = 0x00;
             data[x * 100 * 3 + y * 3 + 2 ] = 0xff;
         }
-    }
+    } */
     
     if(!window){
         printf("could not do it");
         return 1;
     }    
 
+    int frame_width, frame_height;
+    unsigned char* frame_data;
+    if (!load_frame("/Users/Downloads/video_hq.mp4", &frame_width, &frame_height, &frame_data)){
+        printf("Could not load frame");
+        return 1;
+    }
+
     // Create texture
     glfwMakeContextCurrent(window);
     GLuint tex_handle;
+    frame_width = 100;
+    frame_height = 100;
     glGenTextures(1, &tex_handle);
     glBindTexture(GL_TEXTURE_2D, tex_handle);
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    //glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 100, 100, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    //glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_width, frame_height, 0, GL_RGB, GL_UNSIGNED_BYTE, frame_data);
 
     while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -74,14 +85,13 @@ int main (int argc, const char** argv){
         glBindTexture(GL_TEXTURE_2D, tex_handle);
         glBegin(GL_QUADS);
             glTexCoord2d(0,0); glVertex2i(200,200);
-            glTexCoord2d(1,0); glVertex2i(300,200);
-            glTexCoord2d(1,1); glVertex2i(300,300);
-            glTexCoord2d(0,1); glVertex2i(200,300);
+            glTexCoord2d(1,0); glVertex2i(200 + frame_width,200);
+            glTexCoord2d(1,1); glVertex2i(200 + frame_width,200 + frame_height);
+            glTexCoord2d(0,1); glVertex2i(200,200 + frame_height);
         glEnd();
         glDisable(GL_TEXTURE_2D);
         glfwSwapBuffers(window);
         glfwWaitEvents();
     }
-
     return 0;
 }
